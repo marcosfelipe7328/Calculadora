c247dc9403623326f85fee85687a6f4a90a6a346
marcosfelipe7328 <marcosfelipe7328@gmail.com>
2018-01-27 00:30:06 -0200

    melhorias

--- scripts/calculadora.js
+++ scripts/calculadora.js
@@ -1,21 +1,77 @@
 $(document).ready(function() {
     let anulado = true;
-    $('.numero').on('click', function(e) {
-        if (anulado) {
-            $('#resultado').text($(this).text());
-            if ($('#resultado').text() != '0') {
-               anulado = false;  
-            }  
+    var n1;
+    var n2;
+    var operador;
+    $('.numero').on('click', function() {
+        if ($('#resultado').text() == '0' || n1 != null) {
+            $('#resultado').text($(this).text()); 
         }
         else {
             $('#resultado').append($(this).text());
-        } 
+        }
     });
 
     $('#ac').on('click', function() {
-        if (!anulado) {
-            anulado = true;
-            $('#resultado').text('0');
-        }
+        $('#resultado').text('0');
+        n1 = 0;
+        n2 = 0;
     });
+
+    $('#pos-neg').on('click', function() {
+        $('#resultado').text((Number($('#resultado').text() * -1)).toString());
+    });
+
+    $('.operador').on('click', function() {
+        n1 = Number($('#resultado').text());
+        operador = $(this).attr('id');  
+    })
+
+    $('#igual').on('click', function() {
+        n2 = Number($('#resultado').text());
+        calcula();
+    });
+
+    function calcula() {
+        switch (operador) {
+            case 'mais':
+                soma();
+                break;
+            case 'menos':
+                subtrai();
+                break;
+            case 'multiplicacao':
+                multiplica();
+                break;
+            case 'divisao':
+                divide();
+                break;
+            default:
+                break;
+        }
+    }
+
+    function soma() {
+        var resultado = n1 + n2;
+        igual(resultado);
+    }
+
+    function subtrai() {
+        var resultado = n1 - n2;
+        igual(resultado);
+    }
+
+    function multiplica() {
+        var resultado = n1 * n2;
+        igual(resultado);
+    }
+
+    function divide() {
+        var resultado = n1 / n2;
+        igual(resultado);
+    }
+
+    function igual(r) {
+        $('#resultado').text(r.toString());
+    }
 })
